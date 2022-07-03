6e98a83be629bdae20ca1ccd6adfa73728ff42db
Darina Kovalskaya <57908654+holtzdari@users.noreply.github.com>
2022-07-03 20:24:53 +0300

    Make bbox-finder (#2)

--- geoserver/geoserver/urls.py
+++ geoserver/geoserver/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.urls import re_path, path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 
 from gis_app.api import CountryViewSet, CityViewSet
@@ -29,3 +31,7 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     path('gis-api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- geoserver/gis_app/api.py
+++ geoserver/gis_app/api.py
@@ -1,10 +1,38 @@
-from rest_framework import viewsets
+from tokenize import Number
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.gis.geos import Polygon
 from .serializer import CountrySerializer, CitySerializer
 from .models import Country, City
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+    def list(self, request):
+        bbox_query = request.query_params.get('bbox', '')
+        bbox_params = bbox_query.split(',') if bbox_query != '' else []
+        if len(bbox_params) == 4:
+            bbox = (
+                float(bbox_params[0]),
+                float(bbox_params[1]),
+                float(bbox_params[2]),
+                float(bbox_params[3]),
+            )
+            polygon = Polygon.from_bbox(bbox)
+            cities_in_bbox = City.objects.filter(geom__within=polygon)
+            return Response({'detail': CitySerializer(cities_in_bbox, many=True).data}, status=status.HTTP_200_OK)
+        elif len(bbox_params) != 0:
+            return Response({'detail': 'bbox query invalid'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'detail': CountrySerializer(self.queryset, many=True).data}, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        return Response(
+            {'detail': CountrySerializer(get_object_or_404(self.queryset, pk=pk)).data},
+            status=status.HTTP_200_OK
+        )
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
