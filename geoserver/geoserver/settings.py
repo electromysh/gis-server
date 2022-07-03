456b20d1690fd31f3ead4c57cef82ff4b64e05d1
Darina Kovalskaya <57908654+holtzdari@users.noreply.github.com>
2022-07-03 20:22:47 +0300

    Make bbox-finder

--- geoserver/geoserver/settings.py
+++ geoserver/geoserver/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'rest_framework_gis',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- geoserver/gis_app/models.py
+++ geoserver/gis_app/models.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 
 class Country(models.Model):
     name = models.CharField(max_length=255)
-    geom = models.MultiPolygonField(null=True)
+    geom = models.MultiPolygonField(null=True, srid=4326)
 
     def __str__(self) -> str:
         return self.name
@@ -10,9 +10,9 @@ class Country(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    images = models.ImageField()
-    geom = models.MultiPolygonField(null=True)
+    description = models.TextField(null=True, blank=True)
+    images = models.ImageField(null=True, blank=True, upload_to='image')
+    geom = models.MultiPolygonField(null=True, srid=4326)
     isCapital = models.BooleanField(default=False)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
 
