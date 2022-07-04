bcf093b797d8da3569fa10899533128c555bbede
Darina Kovalskaya <57908654+holtzdari@users.noreply.github.com>
2022-07-04 23:10:02 +0300

    Add country query

--- geoserver/gis_app/api.py
+++ geoserver/gis_app/api.py
@@ -1,7 +1,5 @@
-from tokenize import Number
 from rest_framework import viewsets, status
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from django.contrib.gis.geos import Polygon
 from .serializer import CountrySerializer, CitySerializer
 from .models import Country, City
@@ -10,30 +8,41 @@ class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
-    def list(self, request):
-        bbox_query = request.query_params.get('bbox', '')
-        bbox_params = bbox_query.split(',') if bbox_query != '' else []
+    def query_set(self, request):
+        queryset = self.queryset
+
+        bbox_query = request.query_params.get('bbox')
+        bbox_params = bbox_query.split(',') if bbox_query != None else []
+
         if len(bbox_params) == 4:
-            bbox = (
-                float(bbox_params[0]),
-                float(bbox_params[1]),
-                float(bbox_params[2]),
-                float(bbox_params[3]),
-            )
-            polygon = Polygon.from_bbox(bbox)
-            cities_in_bbox = City.objects.filter(geom__within=polygon)
-            return Response({'detail': CitySerializer(cities_in_bbox, many=True).data}, status=status.HTTP_200_OK)
-        elif len(bbox_params) != 0:
-            return Response({'detail': 'bbox query invalid'}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                bbox = (
+                    float(bbox_params[0]),
+                    float(bbox_params[1]),
+                    float(bbox_params[2]),
+                    float(bbox_params[3]),
+                )
+                queryset = City.objects.filter(geom__within=Polygon.from_bbox(bbox))
+            except ValueError:
+                raise ValidationError({'result': 'bbox query is invalid'}, status.HTTP_400_BAD_REQUEST)
+        elif len(bbox_params) != 4 and len(bbox_params) != 0:
+            raise ValidationError({'result': 'bbox query is invalid'}, status.HTTP_400_BAD_REQUEST)
 
-        return Response({'detail': CountrySerializer(self.queryset, many=True).data}, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, pk=None):
-        return Response(
-            {'detail': CountrySerializer(get_object_or_404(self.queryset, pk=pk)).data},
-            status=status.HTTP_200_OK
-        )
+        return queryset
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        country_query = self.request.query_params.get('country')
+
+        if country_query != None:
+            try:
+                country_query = int(country_query)
+                queryset = City.objects.filter(country=country_query)
+            except ValueError:
+                raise ValidationError({'result': 'city id must be integer'}, status.HTTP_400_BAD_REQUEST)
+
+        return queryset
